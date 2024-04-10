ff55e5c0eacbdbeb32746d289fba8f0cace1d9bb
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-11 02:27:14 +0300

    Multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoData, userData]) => {
-      const { firstName, lastName } = userData;
-      console.log(`${photoData.body} ${firstName} ${lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
