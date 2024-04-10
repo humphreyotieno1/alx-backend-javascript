7f23a0f24b67a75b95abc62f58dd23da05f42b2c
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-11 02:08:07 +0300

    Get and Catch methods

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,10 @@ function handleResponseFromAPI(promise) {
   promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
-      console.log('Failed to get a response from the API');
+      console.log('Got a response from the API');
       return new Error();
     });
 }
