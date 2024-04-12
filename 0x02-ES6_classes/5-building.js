768095c8d106d4bc95539c83435beab02f7d3be7
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-13 00:58:35 +0300

    Updating lint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,12 @@
-/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
