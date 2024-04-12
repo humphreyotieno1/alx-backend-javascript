321dd91555e4a866331d65e7dd9e30f8d0123f02
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-13 00:24:31 +0300

    Updating lint

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -9,7 +9,7 @@ export default class EVCar extends Car {
   cloneCar() {
     const clone = new Car();
     Object.getOwnPropertyNames(this).forEach((key) => {
-      if (key !== "cloneCar") {
+      if (key !== 'cloneCar') {
         clone[key] = this[key];
       }
     });
