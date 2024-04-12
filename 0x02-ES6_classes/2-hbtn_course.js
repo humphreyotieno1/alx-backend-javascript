959edc4132a11039ecf7b504fbe48528e4036515
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-13 00:47:42 +0300

    Updating lint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,39 +1,58 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof newName === "string") {
-        this._name = newName;
-      } else {
-        throw new TypeError("Name must be a string");
-      }
-    }
-  
-    get length() {
-      return this._length;
-    }
-  
-    set length(newLength) {
-      if (typeof newLength === "number") {
-        this._length = newLength;
-      } else {
-        throw new TypeError("Length must be a number");
-      }
-    }
-  
-    get students() {
-      return this._students;
-    }
-  
-    set students(newStudents) {
-      this._students = newStudents;
-    }
+  constructor(name, length, students) {
+    this._name = HolbertonCourse._validateString(name, 'Name');
+    this._length = HolbertonCourse._validateNumber(length, 'Length');
+    this._students = HolbertonCourse._validateArray(students, 'Students');
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = HolbertonCourse._validateString(newName, 'Name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = HolbertonCourse._validateNumber(newLength, 'Length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = HolbertonCourse._validateArray(newStudents, 'Students');
+  }
+
+  static _validateString(value, propName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propName} must be a string`);
+    }
+    return value;
+  }
+
+  static _validateNumber(value, propName) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError(`${propName} must be a number`);
+    }
+    return value;
+  }
+
+  static _validateArray(value, propName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${propName} must be an array`);
+    }
+    // Assuming all elements in the array are strings
+    for (const item of value) {
+      if (typeof item !== 'string') {
+        throw new TypeError(`${propName} must contain only strings`);
+      }
+    }
+    return value;
+  }
+}
