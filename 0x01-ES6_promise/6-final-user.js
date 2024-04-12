5cbd9730b0eb33117001835d2e067413490a1760
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-13 01:39:42 +0300

    Correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const userResult = await signUpUser(firstName, lastName);
-    const photoResult = await uploadPhoto(fileName);
-
-    return [
-      { status: 'fulfilled', value: userResult },
-      { status: 'fulfilled', value: photoResult },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', reason: 'Signup system offline' },
-      { status: 'rejected', reason: 'Photo upload failed' },
-    ];
-  }
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+  return [
+    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
+  ];
 }
+export default handleProfileSignup;
