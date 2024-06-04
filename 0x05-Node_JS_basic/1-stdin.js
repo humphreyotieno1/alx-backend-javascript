65ee447e7a504fdd1d0d3a8eba82d76d7c1a9aed
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-05 00:18:31 +0300

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const myLine = process.stdin.read();
+  if (myLine !== null) {
+    process.stdout.write(`Your name is: ${myLine}`);
   }
 });
 
