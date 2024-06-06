61fddce2c2133f78ed3f3bb8a6955b4988b99b6a
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-06 23:13:24 +0300

    Chai module

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,30 @@
+const expect = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should round the numbers and add them', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should round the numbers and subtract', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should round the numbers and divide', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when the second argument is 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+
+  it('should throw an error for an invalid type', () => {
+    expect(() => calculateNumber('MULTIPLY', 1.4, 4.5)).to.throw(/Invalid type/);
+  });
+});
